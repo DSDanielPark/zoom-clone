e5fa33324c77b28022a5c66aa971977c6a255a3d
parkminwoo <parkminwoo1991@gmail.com>
2022-12-27 19:48:59 +0900

    feat: brower connection and socke t method

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,5 +2,14 @@ const socket = new WebSocket(`ws://${window.location.host}`)
 
 
 socket.addEventListener("open", () => {
-    console.log("Conneted to server check!!")
+    console.log("Conneted to server check!!");
 })
+
+socket.addEventListener("message", (message) => {
+    console.log("just got message:", message, "from the server");
+})
+
+
+socket.addEventListener("close", () => {
+    console.log("Conneted from server: X");
+});
