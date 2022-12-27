1acf856b1b26376e242429f57aa760b96c46a9c3
parkminwoo <parkminwoo1991@gmail.com>
2022-12-27 21:08:18 +0900

    feat: submit message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,5 +1,10 @@
-const socket = new WebSocket(`ws://${window.location.host}`)
 
+
+
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+const socket = new WebSocket(`ws://${window.location.host}`)
 
 socket.addEventListener("open", () => {
     console.log("Conneted to server check!!");
@@ -15,7 +20,16 @@ socket.addEventListener("close", () => {
 });
 
 
-// 아무 조건이 없으니까 10초뒤에 그냥 다음 익명함수가 실행되는데 socket.send 이다. 브라우저(프론트)로부터
-setTimeout(() => {
-    socket.send("hello from the brower!!");
-}, 10000);
+// // 아무 조건이 없으니까 10초뒤에 그냥 다음 익명함수가 실행되는데 socket.send 이다. 브라우저(프론트)로부터
+// setTimeout(() => {
+//     socket.send("hello from the brower!!");
+// }, 10000);
+
+
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageForm.querySelector("input");
+    console.log(input.value)
+}
+
+messageForm.addEventListener("submit", handleSubmit)
